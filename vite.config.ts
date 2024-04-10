b533bc409e8659492e10742cefe975caae270380
aline-doleron <doleron.aline@gmail.com>
2024-04-10 15:47:29 -0300

    Test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react-swc'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +13,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
